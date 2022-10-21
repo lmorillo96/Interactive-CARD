c517982b422f9db3ac52e1c6ceda9c4d4729547e
Leandro Morillo <leandro.m9601@gmail.com>
2022-10-20 22:37:47 -0500

    namecard function

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const blank = document.querySelectorAll("input");
 const button = document.querySelector(".btn");
+const customerName = document.querySelector("#cardholder");
 
-blank.forEach((label) => {
+/* blank.forEach((label) => {
   label.addEventListener("keydown", (e) => {
     let codeValue = e.key;
     e.preventDefault();
-    console.log("Su tecla presionada es " + codeValue);
+    codeValue.textContent = "Hola";
   });
 });
+ */
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("Sumbit");
+  nameCard();
 });
+
+function nameCard() {
+  let testArray = customerName.value.split("");
+  let arr2 = [];
+
+  for (let i = 0; i < testArray.length; i++) {
+    //console.log(testArray[i]);
+
+    if (testArray[i] === testArrayykujuk(new RegExp(/[a-z\s]+/))) {
+      arr2.push(testArray);
+    } else {
+      console.log("Solo se permite letras!");
+    }
+  }
+}
